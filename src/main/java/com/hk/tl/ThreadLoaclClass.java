68f99055e5b34a8a54ecd85358df2bc18dcd17fb
kun he <kunhe@kundeMacBook-Pro.local>
2016-07-09 15:40:32 +0800

    git diff test

--- src/main/java/com/hk/tl/ThreadLoaclClass.java
+++ src/main/java/com/hk/tl/ThreadLoaclClass.java
@@ -1,16 +1,19 @@
 package com.hk.tl;
 
 public class ThreadLoaclClass {
-	long val = 0L;
-	
-	public void add()
-	{
-		val++;
-	}
-	
-	public long getVal()
-	{
-		return val;
-	}
+    long val = 0L;
 
+    public void add() {
+        val++;
+    }
+
+    public long getVal() {
+        return val;
+    }
+
+    public void setVal(int i) {
+    }
+
+    public void setValAonther(int i) {
+    }
 }
